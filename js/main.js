f267865b1ba5d2139c60590771bb3f5b3557d9ae
dnazzx <zzan-official@gmail.com>
2020-05-17 22:03:18 +0700

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+    $('.menu-toggler').on('click', function(){
+        $(this).toggleClass('open');
+        $('.top-nav').toggleClass('open');
+    });
+});
+
+buttonUp = document.getElementById("up");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+
